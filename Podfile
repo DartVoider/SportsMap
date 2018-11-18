89834b4ffc6bff23e81f778b42995bbe580c5522
DartVoider <43094872+DartVoider@users.noreply.github.com>
2018-11-18 21:25:33 +0300

    Add files via upload

---
+++ Podfile
@@ -0,0 +1,27 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SportsMap' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  # Основная библиотека MapKit.
+  pod ‘YandexMapKit’, ’~> 3.1’ 
+  # Дополнительные модули MapKit.
+  # Удалите зависимости если ваше приложение не использует их.
+  pod ‘YandexMapKitDirections’, ’~> 3.1’ 
+  pod ‘YandexMapKitPlaces’, ’~> 3.1’ 
+  #pod ‘YandexMapKitSearch’, ’~> 3.1’ 
+  #pod ‘YandexMapKitTransport’, ’~> 3.1’
+  # Pods for SportsMap
+
+  target 'SportsMapTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SportsMapUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ SportsMap/MapViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MapViewController.swift
+//  SportsMap
+//
+//  Created by Sierra on 18.11.2018.
+//  Copyright © 2018 SAD. All rights reserved.
+//
+
+import UIKit
+import YandexMapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet var mapView: YMKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.mapWindow.map.move(
+            with: YMKCameraPosition.init(target: YMKPoint(latitude: 55.751574, longitude: 37.573856), zoom: 15, azimuth: 0, tilt: 0),
+            animationType: YMKAnimation(type: YMKAnimationType.smooth, duration: 5),
+            cameraCallback: nil)
+        // Do any additional setup after loading the view.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
